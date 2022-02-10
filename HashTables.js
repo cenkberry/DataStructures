5daf3f01119d5e69ce6498f0fa862cb0efb61cbb
cenkberry <cenkberi@mail.com>
2022-02-10 23:10:34 +0300

    ok

--- HashTables.js
+++ HashTables.js
@@ -22,11 +22,11 @@ class HashTable {
 
         //if that index didn't created
         if (this.storage[index] === undefined) {
-            this.storage[index] = [
-                [key, value]
-            ];
-
-        } else {
+            this.storage[index] = [ [key, value] ];
+        }
+        
+        else 
+        {
             let inserted = false;
             //if it is in that index,you finding the place in index and changing the value:
             for (let i = 0; i < this.storage[index].length; i++) {
@@ -44,34 +44,47 @@ class HashTable {
 
     remove(key) {
         let index = hash(key, this.sLimit); //create index var.
-        let indexlength = this.storage[index].length; //create index.length
 
         //if there is an index and one key at inside
-        if (indexlength === 1 && this.storage[index][0][0] === key)
-        {
+        if (this.storage[index].length === 1 && this.storage[index][0][0] === key) {
             delete this.storage[index];
-        }
-        else
+        } 
+        else 
         {
             //Create a loop to find the place of key and delete it
-            for (let i = 0; i < this.storage[index]; i++) {
+            for (let i = 0; i < this.storage[index].length; i++) {
                 if (this.storage[index][i][0] === key) {
                     delete this.storage[index][i];
-                } else {
-                    return undefined;
                 }
             }
+        }
+    };
 
+    search(key) {
+        let index = hash(key, this.sLimit); //create index var.
+
+        //if there isnt any key that you search return undefined
+        if (this.storage[index] === undefined) {
+            return undefined;
+        } 
+        else 
+        {
+            //Create a loop to find the place of key and show it
+            for (let i = 0; i < this.storage[index].length; i++) {
+                if (this.storage[index][i][0] === key) {
+                    console.log(this.storage[index][i]);
+                }
+            }
         }
     };
 };
 
+
 const xxx = new HashTable();
 xxx.add("cenkberry", "05456407488");
 xxx.add("ranaberry", "05456407519");
-xxx.add("johnsmith", "05456407955");
-xxx.add("gallardoesteban", "05456407122");
-xxx.add("ranaberry", "05456407522");
-
-xxx.remove("johnsmith");
+xxx.add("zlatanIbrahimovic", "05456407955");
+xxx.add("alvaroRecoba", "05456407122");
+xxx.remove("zlatanIbrahimovic");
 xxx.print();
+xxx.search("ranaberry");
